ab8964a7c7c4327e056e212b4469416ea68f7fe4
Elvis Landi <elandi13@yahoo.com>
2020-09-25 18:46:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,3 @@ seperator = "|"
 lines = "-----------"
 
 puts "#{board[0]} #{seperator} #{board[1]} #{seperator} #{board[2]"
-puts "#{lines}"
-puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
-puts "#{lines}"
-puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
-end
