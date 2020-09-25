8d89e44a0549d13618e38257af4423229750c4f4
Elvis Landi <elandi13@yahoo.com>
2020-09-25 18:33:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,4 +2,4 @@ def display_board(board)
 seperator = "|"
 lines = "-----------"
 
-puts "#{board[0]}, #{seperator}, #{board[1]}, #{seperator}"
+puts "#{board[0]} #{seperator} #{board[1]} #{seperator} #{board[2] "
